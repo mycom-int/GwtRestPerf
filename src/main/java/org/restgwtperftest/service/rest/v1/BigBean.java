2454aafa7c72fa609c2fd07cd3e06b3385e5c947
Ian Tsai <zanyking@gmail.com>
2013-10-26 01:45:06 +0800

    add a bean

---
+++ src/main/java/org/restgwtperftest/service/rest/v1/BigBean.java
@@ -0,0 +1,43 @@
+package org.restgwtperftest.service.rest.v1;
+
+public class BigBean {
+	private String str1;
+	private String str2;
+	private String str3;
+	private String str4;
+	
+	public BigBean() {
+	}
+	public BigBean(String str1, String str2, String str3, String str4) {
+		super();
+		this.str1 = str1;
+		this.str2 = str2;
+		this.str3 = str3;
+		this.str4 = str4;
+	}
+	public String getStr1() {
+		return str1;
+	}
+	public void setStr1(String str1) {
+		this.str1 = str1;
+	}
+	public String getStr2() {
+		return str2;
+	}
+	public void setStr2(String str2) {
+		this.str2 = str2;
+	}
+	public String getStr3() {
+		return str3;
+	}
+	public void setStr3(String str3) {
+		this.str3 = str3;
+	}
+	public String getStr4() {
+		return str4;
+	}
+	public void setStr4(String str4) {
+		this.str4 = str4;
+	}
+	
+}
